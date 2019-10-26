954f634b9a0cce9225b97a686c5470424a88f3c7
joaomsal <joaomsal@outlook.com>
2019-10-26 00:46:16 +0000

    Teste 02

--- MeuProjetoTeste/src/main/java/br/ufs/dcomp/MeuProjeto/App.java
+++ MeuProjetoTeste/src/main/java/br/ufs/dcomp/MeuProjeto/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "------SISTEMAS DISTRIBUIDOS ------" );
+        System.out.println( "------ AULA 02 - TESTE  ------" );
     }
 }
